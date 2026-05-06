ab84c3214ee2fc0f4d81e915800d12d7d605f9a7
Passionatecoder20230412 <anegondivijay9490@gmail.com>
2026-05-06 22:13:01 +0530

    Switch from Edge to Chrome for WebDriver
    Updated the driver initialization to use Chrome instead of Edge and modified options for Jenkins compatibility.

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.edge.options import Options
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 CURRENT_URL = "https://www.amazon.in/"
 
@@ -9,22 +11,24 @@ def init_driver():
 
     options = Options()
 
-    # IMPORTANT FOR JENKINS
+    # 🔥 REQUIRED for Jenkins / CI
     options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Edge(options=options)
+    # start chrome safely
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
 
-    driver.maximize_window()
     driver.implicitly_wait(10)
     driver.get(CURRENT_URL)
 
     yield driver
 
-    print("closing browser")
     driver.quit()
 # import pytest
 # from playwright.sync_api import sync_playwright
